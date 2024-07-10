12a990baaab7608bb4a38c377bdc64d05c6da2c0
caleholst <caleholst@gmail.com>
2024-07-10 11:32:05 -0500

    Added Input Validation

--- ModelBinding/Models/Ticket.cs
+++ ModelBinding/Models/Ticket.cs
@@ -10,12 +10,14 @@ namespace ModelBinding.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Please enter a name.")]
+        [RegularExpression(@"^[a-zA-Z\s]*$", ErrorMessage = "Name can only contain letters and spaces.")] 
         public string Name { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Please enter a description.")]
         public string Description { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Please select a due date.")]
+        [FutureDate(ErrorMessage = "Due date must be in the future.")]
         public DateTime? DueDate { get; set; }
 
         [Required(ErrorMessage = "Please select a category.")]
@@ -48,5 +50,17 @@ namespace ModelBinding.Models
     {
         public string StatusId { get; set; }
         public string Name { get; set; }
+    }
+
+    public class FutureDateAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            if (value is DateTime dateTime)
+            {
+                return dateTime > DateTime.Now;
+            }
+            return true;
+        }
     }
 }
