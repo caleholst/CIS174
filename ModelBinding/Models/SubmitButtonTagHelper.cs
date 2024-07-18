7685e850a9ae50a9f682f4402e1f0b05b5057b03
caleholst <caleholst@gmail.com>
2024-07-18 12:01:36 -0500

    Did assignment "Website Add Tag Helper & View Components"

---
+++ ModelBinding/Models/SubmitButtonTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace ModelBinding.TagHelpers
+{
+    [HtmlTargetElement("submit-button")]
+    public class SubmitButtonTagHelper : TagHelper
+    {
+        public string Text { get; set; } = "Submit";
+        public string Class { get; set; } = "btn btn-primary";
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "button";
+            output.Attributes.SetAttribute("type", "submit");
+            output.Attributes.SetAttribute("class", Class);
+            output.Content.SetContent(Text);
+        }
+    }
+}
---
+++ ModelBinding/ViewComponents/StatusButtonsViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ModelBinding.ViewComponents
+{
+    public class StatusButtonsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(string currentStatus)
+        {
+            var statuses = new List<string> { "To Do", "In Progress", "Quality Assurance (QA)", "Done" };
+            ViewBag.CurrentStatus = currentStatus;
+            return View(statuses);
+        }
+    }
+}
